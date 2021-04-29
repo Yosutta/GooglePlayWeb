ba9fa4c76844776934a2280ed134ef0f2ecd9493
Yosutta <phuphuongtin@gmail.com>
2021-04-29 12:29:12 +0700

    Small update

--- source/info_update.php
+++ source/info_update.php
@@ -101,7 +101,7 @@
                                 <td>
                                     <?php
                                         if($row['gender']==1)
-                                            echo '<input type="radio" id="male_radio" name="gender_radio" value=1 checked disabled>';
+                                            echo '<input type="radio" id="male_radio" name="gender_radio" value="1" checked disabled>';
                                         else
                                             echo '<input type="radio" id="male_radio" name="gender_radio" disabled>'
                                     ?>
@@ -110,7 +110,7 @@
                                 <td>
                                     <?php
                                         if($row['gender']==0)
-                                            echo '<input type="radio" id="female_radio" name="gender_radio" value=0 checked disabled>';
+                                            echo '<input type="radio" id="female_radio" name="gender_radio" value="0" checked disabled>';
                                         else
                                             echo '<input type="radio" id="female_radio" name="gender_radio" disabled>'
                                     ?>
@@ -220,20 +220,22 @@
 
         $fullname = $_POST['fullname'];
         $birthDate = $_POST['birthday'];
-        $gender = $_POST["gender_radio"];
-        $birthDate = $_POST["birthday"];
         $country = $_POST["nation"];
         $phoneNumber = $_POST["phone"];
 
+        if($_POST['gender_radio']=='on')
+            $gender = 1;
+        else
+            $gender = 0;
 
         // $sql = "UPDATE usersinfo SET `pictureLink`='$targets' WHERE userid='$userid'";
-        $sql = "UPDATE `usersinfo` SET `fullName`='$fullname',`birthDate`='$birthDate',`gender`=$gender,`country`='$country',`phoneNumber`='$phoneNumber',`pictureLink`='$targets' WHERE userid='$userid'";
-        
+        $sql = "UPDATE usersinfo SET `fullName`='$fullname',`birthDate`='$birthDate',`gender`=$gender,`country`='$country',`phoneNumber`='$phoneNumber',`pictureLink`='$targets' WHERE userid=$userid";
         $conn->query($sql);
+
+        echo $sql;
 
         include("updateSession.php");
         updateAccount($userid);
-        
     }
 ?>
 </html>
