17f4119d9e4964ea7b917743706a4b073607f56c
Eric Dang <ericdang2709@gmail.com>
2021-05-14 13:17:03 +0700

    update featured_apps

---
+++ source/fragmentSeeMoreElement.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GooglePlay</title>
+    <?php
+        session_start();
+        echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>";
+        echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>";
+        echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js'>";
+        echo "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'></script>";
+        echo "<script src='https://kit.fontawesome.com/a076d05399.js'></script>";
+        echo "<link href='https://fonts.googleapis.com/css?family=Inter:400,800,900&display=swap' rel='stylesheet'>";
+        echo "<link rel='icon' href='resources/icon.png'>";
+        echo "<script src='main.js'></script>";
+        echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "database";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password,$dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        
+        $sql = "SELECT * FROM `apps`";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        
+    ?>    
+</head>
+<body>
+    <div id="div_seemore_fragment">
+        <div style="width: 500px;">
+        <p class="title_seemore">Title</p>
+        <button type="submit" class="btn btn-success button_seemore">See More</button>
+        </div>
+    </div>
+</body>
+</html>
